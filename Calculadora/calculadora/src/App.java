eb5542052953a9cbe0c3fb6d287be20efd62a77a
El-Vagabond <pesozano@gmail.com>
2025-02-20 13:45:13 +0100

    modified:   Calculadora/calculadora/src/App.java

--- Calculadora/calculadora/src/App.java
+++ Calculadora/calculadora/src/App.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Primer commit!");
+
+        Scanner escaner = new Scanner(System.in);
     }
 }
